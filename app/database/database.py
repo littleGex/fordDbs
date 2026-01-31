9d11853fa4f9431b198d2d0f1c0675186b5e671e
littleGex <67484931+littleGex@users.noreply.github.com>
2026-01-31 17:22:59 +0100

    fixup! feat: Pocket money endpoints

--- app/database/database.py
+++ app/database/database.py
@@ -2,10 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://pi_user:password@db:5432/postgres")
+
+load_dotenv()
+
+
+def get_db_url():
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST", "localhost")  # Defaults to localhost for PyCharm
+    port = os.getenv("DB_PORT", "5432")
+    db_name = os.getenv("DB_NAME")
+
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+DATABASE_URL = get_db_url()
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,
