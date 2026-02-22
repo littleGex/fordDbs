229341cc19ea38cd6a33165b3694d4b855b3b73f
littleGex <67484931+littleGex@users.noreply.github.com>
2026-02-22 19:29:32 +0100

    fixup! frontend: profilepicker

--- family-photos-frontend/src/views/ProfilePicker.vue
+++ family-photos-frontend/src/views/ProfilePicker.vue
@@ -3,15 +3,14 @@
     <h1>Who's watching?</h1>
     <div class="user-grid">
       <div
-        v-for="user in users"
-        :key="user.id"
-        class="profile-card"
-        @click="selectUser(user)"
+          v-for="user in users"
+          :key="user.id"
+          class="profile-card"
+          @click="selectUser(user)"
       >
         <img
-          :src="user.profile_photo_url"
-          :alt="user.display_name"
-          class="avatar"
+            :src="'http://ford-home-pi.local:9000/family-photos/' + user.profile_photo_key"
+            class="avatar"
         />
         <h3>{{ user.display_name }}</h3>
       </div>
@@ -20,9 +19,9 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import {ref, onMounted} from 'vue';
 import api from '../api/axios';
-import { useAuthStore } from '../stores/auth';
+import {useAuthStore} from '../stores/auth';
 
 const auth = useAuthStore();
 const users = ref([]);
