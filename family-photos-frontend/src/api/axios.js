b75a75695275899922ebb41b6100e7b05aa24624
littleGex <67484931+littleGex@users.noreply.github.com>
2026-02-25 07:52:52 +0100

    bug: photo upload issues 4

--- family-photos-frontend/src/api/axios.js
+++ family-photos-frontend/src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Vite uses import.meta.env to access .env variables
   baseURL: import.meta.env.VITE_API_BASE,
-  headers: {
-    'Content-Type': 'application/json',
-  }
 });
 
 export default api;
