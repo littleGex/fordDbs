035e270fe5e1ae74135e8a0f837fa07a9b03bb76
littleGex <67484931+littleGex@users.noreply.github.com>
2026-03-07 17:36:22 +0100

    fixup! feature: Improve frontend

--- utilities-frontend/src/App.vue
+++ utilities-frontend/src/App.vue
@@ -5,12 +5,3 @@
 <script setup>
 import Dashboard from './views/Dashboard.vue'
 </script>
-
-<style>
-/* Add Tailwind imports in your main.css, but standard resets here */
-body {
-  @apply bg-gray-50;
-  margin: 0;
-  font-family: 'Inter', sans-serif;
-}
-</style>
